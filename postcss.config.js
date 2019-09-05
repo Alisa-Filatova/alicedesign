6761002a6e6e71c90b6945cf1c5e1f4bcff2a449
Alisa Filatova <alicefill@bk.ru>
2019-09-05 17:36:48 +0300

    updated index en version

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const autoprefixer = require('autoprefixer');
+const flexbugsFixes = require('postcss-flexbugs-fixes');
+
+module.exports = {
+    plugins: [
+        // https://github.com/luisrudge/postcss-flexbugs-fixes
+        flexbugsFixes,
+
+        // https://github.com/postcss/autoprefixer
+        autoprefixer({
+            // will add prefixes only for final and IE versions of specification
+            flexbox: 'no-2009',
+
+        }),
+    ],
+};
