a710428b1e065f0b49d6384a129a787576082c08
Alisa Filatova <alicefill@bk.ru>
2025-01-11 16:20:29 +0300

    updated text

--- js/code-en.js
+++ js/code-en.js
@@ -316,7 +316,6 @@ $(function() {
 		description: [
 			'<p>Landing page about the Vasyuki tournament. Based on the story "12 Chairs" by Ilf and Petrov.',
        'Adaptive layout was done with vanilla HTML/CSS/JS, without the use of preprocessors, builders and JS plugins. I used CSS Nesting, :has, blend-mode, CSS variables and other modern CSS properties. </p>' + 
-			'<p>The purpose of the site is a test task for Yandex Crowd (I passed the test, but refused further interviews myself, since I did not agree on the terms and conditions).</p>' +
 			'<p><b>Stack</b>: HTML5/CSS3, JavaScript, Figma, Adobe Photoshop.<br>'+
 			'<b>Browser support</b>: Chrome, Firefox.</p>'
 		].join(' '),
